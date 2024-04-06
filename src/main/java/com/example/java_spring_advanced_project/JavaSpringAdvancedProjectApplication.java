9570bcf462e20abfa8f4fdaf0911a47394a9e6d9
Da4ko <da4ko06@gmail.com>
2024-04-06 10:35:47 +0300

    html and css transfered in intelij

---
+++ src/main/java/com/example/java_spring_advanced_project/JavaSpringAdvancedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_advanced_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAdvancedProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringAdvancedProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/java_spring_advanced_project/web/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.java_spring_advanced_project.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+
+        return "home_not_logged_in";
+    }
+}
